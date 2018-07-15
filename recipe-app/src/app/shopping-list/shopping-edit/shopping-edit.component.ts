0f5f6a30e1aa2dd3175e0c4a56cb30c404ac38c1
Akram <azaiezakrem@gmail.com>
2018-07-15 00:01:15 -0700

    implemented component adding feature

--- recipe-app/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ recipe-app/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild, EventEmitter, Output } from '@angular/core';
+import { Ingredient } from '../../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -8,6 +9,7 @@ import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
 export class ShoppingEditComponent implements OnInit {
   @ViewChild('nameInput') nameInputRef: ElementRef;
   @ViewChild('amountInput') amountInputRef: ElementRef;
+  @Output() ingredientAdded = new EventEmitter <Ingredient>();
 
   constructor() { }
 
@@ -15,7 +17,8 @@ export class ShoppingEditComponent implements OnInit {
   }
 
   onAddItem() {
-
+    const newIngredient = new Ingredient(this.nameInputRef.nativeElement.value, this.amountInputRef.nativeElement.value);
+    this.ingredientAdded.emit(newIngredient);
   }
 
 }
