824d8a9577071660c9943d56aa47e4a29dde1ccf
Akram <azaiezakrem@gmail.com>
2018-07-18 16:01:57 -0700

    completed implementation of unless temaplate

--- directives-start/src/app/unless.directive.ts
+++ directives-start/src/app/unless.directive.ts
@@ -1,10 +1,19 @@
-import { Directive } from '@angular/core';
+import { Directive, Input, TemplateRef, ViewContainerRef } from '@angular/core';
 
 @Directive({
   selector: '[appUnless]'
 })
 export class UnlessDirective {
+  // this still is a property, it's just the setter of a property which is a method
+  // that gets  executed whenever the property changes (usually outside of this drective)
+  @Input() set appUnless(condition: boolean) {
+    if (!condition) {
+      this.vcRef.createEmbeddedView(this.templateRef);
+    } else {
+      this.vcRef.clear();
+    }
+  }
 
-  constructor() { }
+  constructor(private templateRef: TemplateRef<any>, private vcRef: ViewContainerRef) { }
 
 }
