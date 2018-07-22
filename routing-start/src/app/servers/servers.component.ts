c41e6a4d61c36d47a7d443eb295af9a8d64e98fd
Akram <azaiezakrem@gmail.com>
2018-07-21 17:17:29 -0700

    added observable to capture url change

--- routing-start/src/app/servers/servers.component.ts
+++ routing-start/src/app/servers/servers.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ServersService } from './servers.service';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-servers',
@@ -9,10 +10,20 @@ import { ServersService } from './servers.service';
 export class ServersComponent implements OnInit {
   private servers: {id: number, name: string, status: string}[] = [];
 
-  constructor(private serversService: ServersService) { }
+  constructor(private serversService: ServersService, private router: Router,
+    // if we want to add a route on top of the current route, we use Activated route below
+    // private route: ActivatedRoute
+  ) { }
 
   ngOnInit() {
     this.servers = this.serversService.getServers();
   }
 
+  onReload() {
+    this.router.navigate(['/servers']);
+    // unlike the routerLink, the navigate method does not know what route we are currently on,
+    // so to tell the navigate method where we currently are, we need to inject active route and
+    // add an argument to navigate, see below
+    // this.router.navigate(['servers'], {relativeTo: route});
+  }
 }
--- routing-start/src/app/users/user/user.component.ts
+++ routing-start/src/app/users/user/user.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
 
 @Component({
   selector: 'app-user',
@@ -8,9 +9,23 @@ import { Component, OnInit } from '@angular/core';
 export class UserComponent implements OnInit {
   user: {id: number, name: string};
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.user = {
+      id: this.route.snapshot.params['id'],
+      name: this.route.snapshot.params['name']
+    };
+    this.route.params
+    // observables are a feature added by a third party package tha allow working with asynchronous
+    // tasks. An observable is an easy way to subscribe to an event that might happen in the future
+    // and then execute some code when it happens
+      .subscribe(
+        (params: Params) => {
+          this.user.id = params['id'];
+          this.user.name = params['name'];
+        }
+      );
   }
 
 }
